bc62ee55575451e8b46d99afd7d2ea04f585c8f7
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-02 16:19:03 +0600

    countries name and id  create json data add server

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const countries = require('./data/countries.json');
+
 app.get('/',(req,res)=> {
     res.send('Hello,chef is running');
+})
+
+app.get('/countries',(req,res) => {
+    res.send(countries);
 })
 
 
