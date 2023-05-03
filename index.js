8f9fc949dafe0d239384981ac7c072fd91fe3813
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-03 06:59:56 +0600

    add same picture set

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const countries = require('./data/countries.json');
+const chefs = require('./data/chefs.json');
 
-app.get('/',(req,res)=> {
+app.use(cors());
+
+app.get('/', (req, res) => {
     res.send('Hello,chef is running');
 })
 
-app.get('/countries',(req,res) => {
+app.get('/countries', (req, res) => {
     res.send(countries);
 })
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedChefs = chefs.find(chef =>chef._id === id);
+    res.send(selectedChefs);
+})
 
-app.listen(port,() => {
+app.get('/countries/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(chefs);
+    } else {
+        const countryChefs = chefs.filter(chef => parseInt(chef.category_id) === id);
+        res.send(countryChefs);
+    }
+})
+
+
+app.listen(port, () => {
     console.log(`chef API  is running on port : ${port}`);
 })
